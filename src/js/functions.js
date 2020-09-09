1f7777d010fb1fabcb23d19d8d9af76fbb79a098
rocolado <r.colado.chaves@gmail.com>
2020-09-09 20:40:47 +0200

    1a subida

---
+++ src/js/functions.js
@@ -0,0 +1,20 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  slideIndex = n;
+  console.log("slides: ", slides)
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  console.log("slideobjetivo: ", slides[slideIndex-1])
+  slides[slideIndex-1].style.display = "block";
+}
